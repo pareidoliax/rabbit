520fc790a60e53efe9f247693e83af038e74e4a6
simpsonness <corallishan@gmail.com>
2014-02-10 19:39:39 +0800

    Associated users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :comments
-  attr_accessible :body, :title
+	belongs_to :user
+    attr_accessible :body, :title
+
+    default_scope order('created_at DESC')
 end
